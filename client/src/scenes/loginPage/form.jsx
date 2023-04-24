89864507c653e7c5e35649391ce1842ac1041dc1
momo01055789 <mohamed.elhadidy666@gmail.com>
2023-04-24 21:08:04 +0200

    second changes

--- client/src/scenes/loginPage/form.jsx
+++ client/src/scenes/loginPage/form.jsx
@@ -80,11 +80,14 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(values),
-    });
+    const loggedInResponse = await fetch(
+      "https://social-app-m8sl.onrender.com/auth/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      }
+    );
     const loggedIn = await loggedInResponse.json();
     if (!loggedIn.msg) {
       dispatch(
